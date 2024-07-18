f0c1b30c219a441042f57fb80b308e35c1746ed8
mpaez <paezmorayviviana@hotmail.com>
2024-07-17 23:07:20 -0300

    Cambios recientes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from "express";
+import { engine } from "express-handlebars";
+import { Server } from "socket.io";
 import productsRouter from "./routes/products.router.js";
 import cartsRouter from "./routes/carts.router.js";
+import viewsRouter from "./routes/views.router.js";
 
 const app = express();
 const puerto = 8080;
@@ -8,11 +11,46 @@ const puerto = 8080;
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("./src/public")); 
+
+//Express-Handlebars
+app.engine("handlebars", engine()); 
+app.set("view engine", "handlebars"); 
+app.set("views", "./src/views"); 
 
 // Rutas
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
+app.use("/", viewsRouter);
 
-app.listen(puerto, () => {
+const httpServer = app.listen(puerto, () => {
     console.log(`Servidor escuchando en el puerto ${puerto}`);
 });
+
+import ProductManager from "./controllers/product-manager.js";
+const productManager = new ProductManager("./src/models/productos.json");
+
+const io = new Server(httpServer); 
+
+
+io.on("connection", async (socket) => {
+    console.log("Un cliente se conecto"); 
+
+    //Enviamos el array de productos: 
+    socket.emit("productos", await productManager.getProducts());
+
+    //Recibimos el evento "eliminarProducto" desde el cliente: 
+    socket.on("eliminarProducto", async (id) => {
+        await productManager.deleteProduct(id);
+
+        //Le voy a enviar la lista actualizada al cliente: 
+        io.sockets.emit("productos", await productManager.getProducts());
+    })
+
+    //Agregamos productos por medio de un formulario: 
+    socket.on("agregarProducto", async (producto) => {
+        await productManager.addProduct(producto); 
+        //Le voy a enviar la lista actualizada al cliente: 
+        io.sockets.emit("productos", await productManager.getProducts());
+    })
+})
